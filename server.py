906299c9354db3a2ec2700e782cc4c52ea6914f4
hodaypi <62617707+hodaypi@users.noreply.github.com>
2023-05-09 15:15:37 +0300

    The server is ready to use add TRY and EXPERT added comments where necessary divide to functions for repeating checks Just to delete unnecessary comment and prints

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ import pyrebase
 # from collections.abc import MutableMapping
 # from pyrebase import initialize_app
 
+# connect to firebase Databese:
+
 config = {
     "apiKey": "AIzaSyD_LSxNAWSX0wn0NXFVFi-W6mwfqj7HhzM",
     "authDomain": "hmocovidmgmtsyst.firebaseapp.com",
@@ -20,6 +22,7 @@ config = {
     "messagingSenderId": "696678208500",
     "appId": "1:696678208500:web:68571b0e2162e0f188dfa1"
 }
+
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
@@ -173,7 +176,6 @@ db = firebase.database()
 #         print(date.val())
 
 
-
 #
 #         if request.form['get' + str(i)] == "Yes" and check == 0:
 #                             vaccineDate = request.form['vaccineDate' + str(i)]
@@ -192,67 +194,96 @@ db = firebase.database()
 
 app = Flask(__name__)
 
-def empty(detail):
-    if detail == "":
-        return True
 
+# func check if the field is empty or not
+def fieldIsFull(detail):
+    if detail != "":
+        return True
+    else:
+        return False
+
+
+# func check if the data is exist or not
+def dataExist(val):
+    if val is not None:
+        return True
+    else:
+        return False
+
+
+# main screen- this page shown first
 @app.route('/', methods=['GET', 'POST'])
 def welcomeScreen():
-    if request.method == 'POST':
-        if request.form['submit'] == 'Add':
-            return render_template('addNewClient.html')
-        elif request.form['submit'] == 'Search':
-            return render_template('findClient.html')
+    try:
+        if request.method == 'POST':
+            # When the user chooses to add new client, it redirects to fit page
+            if request.form['submit'] == 'Add':
+                return render_template('addNewClient.html')
+            # When the user chooses to get specific client details, it redirects to fit page
+            elif request.form['submit'] == 'Search':
+                return render_template('findClient.html')
+    except:
+        someError = "An error occurred. Check the connection"
+        return render_template("welcomeScreen.html", failure=someError)
     return render_template("welcomeScreen.html")
 
 
+# When the user chooses to add a new client, it redirects to this page and activates addNewClient() func
 # //addNewClient
 @app.route('/addNewClient', methods=['GET', 'POST'])
 def addNewClient():
-    # print("func of adding new client")
-    if request.method == 'POST':
-        if request.form['submit'] == 'add':
-            clientId = request.form['id']
-            # client already exist id.val != None
-            if db.child("Clients Details").child(clientId).get().val() is not None:
-                # print("This client already exist")
-                existClient = "The client already exists in the system, try to enter another ID"
-                return render_template('addNewClient.html', failure=existClient)
-            else:
-                db.child("Clients Details").child(clientId).set(
-                    {
-                        "firstName": request.form['fname'],
-                        "lastName": request.form['lname'],
-                        "birthday": request.form['bday'],
-                        "address": request.form['street'] + " " + request.form['number'] + ", " + request.form['city'],
-                        "telephone": request.form['tel'],
-                        "cellPhone": request.form['phone']})
-                if request.form['positiveDate'] != "":
-                    db.child("Clients Details").child(clientId).update(
-                        {"positiveCovidDate": request.form['positiveDate']})
-                if request.form['recoveryDate'] != "":
-                    db.child("Clients Details").child(clientId).update(
-                        {"covidRecoveryDate": request.form['recoveryDate']})
-
-                check = 0
-                for i in range(1, 5):
-                    if request.form['get' + str(i)] == "Yes" and check == 0:
-                        vaccineDate = request.form['vaccineDate' + str(i)]
-                        if request.form['producer' + str(i)] == "Pfizer":
-                            idOfVaccine = db.child("Covid's Vaccines").child(str(i)+'p').get()
-                        elif request.form['producer' + str(i)] == "Moderna":
-                            idOfVaccine = db.child("Covid's Vaccines").child(str(i)+'m').get()
+    try:
+        if request.method == 'POST':
+            if request.form['submit'] == 'add':
+                clientId = request.form['id']
+                # client already exist id.val != None
+                if dataExist(db.child("Clients Details").child(clientId).get().val()):
+                    existClient = "The client already exists in the system, try to enter another ID"
+                    return render_template('addNewClient.html', failure=existClient)
+                else:
+                    # insert personal details, and set the ID as unique key
+                    db.child("Clients Details").child(clientId).set(
+                        {
+                            "firstName": request.form['fname'],
+                            "lastName": request.form['lname'],
+                            "birthday": request.form['bday'],
+                            "address": request.form['street'] + " " + request.form['number'] + ", " + request.form['city'],
+                            "telephone": request.form['tel'],
+                            "cellPhone": request.form['phone']})
+                    # insert the date of the illness with Covid and the date of recovery if they exist
+                    if fieldIsFull(request.form['positiveDate']):
+                        db.child("Clients Details").child(clientId).update(
+                            {"positiveCovidDate": request.form['positiveDate']})
+                        if fieldIsFull(request.form['recoveryDate']):
+                            db.child("Clients Details").child(clientId).update(
+                                {"covidRecoveryDate": request.form['recoveryDate']})
+                    # insert the date of getting covid's vaccines if exist
+                    check = 0
+                    # Client can get a maximum of 4 vaccines
+                    for i in range(1, 4):
+                        if request.form['get' + str(i)] == "Yes" and check == 0:
+                            vaccineDate = request.form['vaccineDate' + str(i)]
+                            if request.form['producer' + str(i)] == "Pfizer":
+                                idOfVaccine = db.child("Covid's Vaccines").child(str(i) + 'p').get()
+                            elif request.form['producer' + str(i)] == "Moderna":
+                                idOfVaccine = db.child("Covid's Vaccines").child(str(i) + 'm').get()
+                            else:
+                                idOfVaccine = db.child("Covid's Vaccines").child(str(i) + 'a').get()
+                            db.child("Clients Details").child(clientId).child("Vaccines").child(idOfVaccine.key()).set(
+                                {"vaccineDate": vaccineDate})
+                        # If client didn't get the first vaccine- he didn't get the next ones either
                         else:
-                            idOfVaccine = db.child("Covid's Vaccines").child(str(i)+'a').get()
-                        db.child("Clients Details").child(clientId).child("Vaccines").child(idOfVaccine.key()).set(
-                            {"vaccineDate": vaccineDate})
-                    # #  אם לא עשה את הראשון אז מן הסתם שלא עשה את השאר
-                    else:
-                        check = 1
-                return render_template('welcomeScreen.html')
+                            check = 1
+                    # if success return to main screen
+                    return render_template('welcomeScreen.html')
+    # If there is a problem adding the user to the database/ there is a problem with the data
+    except:
+        error = "An error occurred, check the database/server connection and try again"
+        return render_template('addNewClient.html', failure=error)
     return render_template('addNewClient.html')
 
 
+# When the user chooses to get specific client details, it redirects to this page and activates findClient() func
 #  //clientDetails
 @app.route('/findClient', methods=['GET', 'POST'])
 def findClient():
@@ -264,53 +295,54 @@ def findClient():
                 client = db.child("Clients Details").child(idOfClient).get()
                 clientID = client.key()
                 clientD = client.val()
-                personalDetails =[clientD["firstName"] + " " + clientD["lastName"], clientID, clientD["address"], clientD["birthday"],clientD["telephone"],clientD["cellPhone"]]
+                personalDetails = [clientD["firstName"] + " " + clientD["lastName"],
+                                   clientID,
+                                   clientD["address"],
+                                   clientD["birthday"],
+                                   clientD["telephone"],
+                                   clientD["cellPhone"]]
 
                 gotCovid = db.child("Clients Details").child(idOfClient).child("positiveCovidDate").get()
-                if gotCovid.val() is None:
+                if not dataExist(gotCovid.val()):
                     getCovid = "Did'nt get covid"
                 else:
                     getCovid = gotCovid.val()
 
                 finishCovid = db.child("Clients Details").child(idOfClient).child("covidRecoveryDate").get()
-                if finishCovid.val() is None:
+                if not dataExist(finishCovid.val()):
                     recoverCovid = "Did'nt heal covid"
                 else:
                     recoverCovid = finishCovid.val()
 
                 getVaccine = db.child("Clients Details").child(idOfClient).child("Vaccines").get()
-                if getVaccine.val() is None:
-                    vaccineCovid="Did'nt take vaccine"
-                    vaccineList=[]
+                if not dataExist(getVaccine.val()):
+                    vaccineCovid = "Did'nt take vaccine"
+                    vaccineList = []
                 else:
                     vaccineCovid = "Got vaccine"
                     clientVaccine = getVaccine.val()
-                    print(clientVaccine)
-                    print(clientVaccine.values())
                     vaccineList = []
                     for i in clientVaccine.keys():
-                        list =[]
+                        vList = []
                         booster = db.child("Covid's Vaccines").child(i).child("vaccineDose").get()
-                        list.append(booster.val())
+                        vList.append(booster.val())
 
                         producer = db.child("Covid's Vaccines").child(i).child("vaccineProducer").get()
-                        list.append(producer.val())
+                        vList.append(producer.val())
 
-                        date = db.child("Clients Details").child(idOfClient).child("Vaccines").child(i).child("vaccineDate").get()
-                        list.append(date.val())
+                        date = db.child("Clients Details").child(idOfClient).child("Vaccines").child(i).child(
+                            "vaccineDate").get()
+                        vList.append(date.val())
 
-                        vaccineList.append(list)
-                    # print(vaccineList)
+                        vaccineList.append(vList)
+                # if success redirect to ClientDetails page that shown the client card
                 return render_template('ClientDetails.html',
                                        personal=personalDetails,
                                        sick=getCovid,
                                        healthy=recoverCovid,
                                        vaccine=vaccineCovid,
                                        listOfVaccine=vaccineList)
-            # successkeys = clientD.keys(),
-            # successValues = clientD.values(),
-            #
-            # all = clientD.items()
+            # If the client does not exist / there is a problem and cannot bring details
             except:
                 noClient = "The client is not exist in the system. Try to enter another ID"
                 return render_template('findClient.html', failure=noClient)
@@ -320,4 +352,3 @@ def findClient():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
